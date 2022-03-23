434e0d87f171e58784341ee4075050c6c255574d
Sanne Esajas <302560545@student.rocmondriaan.nl>
2022-03-23 16:20:45 +0100

    new pages

--- htdocs/defaults/head.php
+++ htdocs/defaults/head.php
@@ -1,1 +1,12 @@
-<?php
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/bootstrap-5/css/bootstrap.css">
+    <script src="https://kit.fontawesome.com/f4cc39589e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/public/bootstrap-5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/public/bootstrap-5/css/main.css">
+    <script src="/public/bootstrap-5/js/bootstrap.bundle.min.js"></script>
+    <script src="/public/bootstrap-5/js/bootstrap.js"></script>
+    <title>HBO Voorlichting</title>
+</head>
---
+++ htdocs/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include_once('defaults/head.php');
+?>
+<body>
+<main>
+    <div class="container">
+
+        <h1>Hello</h1>
+        </div>
+        <div class="toggle-header-close">
+            <svg width="17" height="17" viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg"><title>Close icon</title><g fill="#FFF" fill-rule="evenodd"><path d="M1.377 1.3C2 .68 3.012.683 3.624 1.295L15.658 13.33c.62.618.62 1.62-.007 2.246-.62.622-1.633.62-2.246.007L1.37 3.548C.75 2.93.75 1.928 1.377 1.3z"></path><path d="M1.338 15.675c-.623-.622-.62-1.635-.007-2.247L13.366 1.393c.62-.618 1.62-.62 2.247.007.623.623.62 1.635.007 2.247L3.584 15.682c-.62.618-1.62.62-2.247-.007z"></path></g></svg>
+        </div>
+
+        <nav class="block-sectorenList">
+            <ol class="no-increment">
+                <li class="prefix">
+                    Iets met...
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/beauty-hair" class="no-arrow">Beauty &amp; Hair</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/business" class="no-arrow">Business</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/evenementen" class="no-arrow">Evenementen</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/facilitair" class="no-arrow">Facilitair</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/fashion" class="no-arrow">Fashion</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/horeca" class="no-arrow">Horeca</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/ict" class="no-arrow">ICT</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/logistics" class="no-arrow">Logistics</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/musical" class="no-arrow">Musical</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/onderwijs" class="no-arrow">Onderwijs</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/retail" class="no-arrow">Retail</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/sport-en-bewegen" class="no-arrow">Sport en Bewegen</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/techniek" class="no-arrow">Techniek</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/toerisme-en-recreatie" class="no-arrow">Toerisme en Recreatie</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/veiligheid" class="no-arrow">Veiligheid</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/welzijn" class="no-arrow">Welzijn</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/zorg" class="no-arrow">Zorg</a>
+                </li>
+            </ol>
+            <ol class="no-increment">
+                <li class="">
+                    <a href="https://www.rocmondriaan.nl/bas" class="no-arrow">BAS (Begeleiding, Advies, Schakeltrajecten)</a>
+                </li>
+                <li class="vavoLink">
+                    <a href="https://www.rocmondriaan.nl/school-voor-entree" class="no-arrow">Entree-onderwijs</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/school-voor-vavo" class="no-arrow">VAVO (mavo-havo-vwo)</a>
+                </li>
+                <li>
+                    <a href="https://www.rocmondriaan.nl/roc-mondriaan-taal-school" class="no-arrow">Volwassenenonderwijs</a>
+                </li>
+
+            </ol>
+        </nav>
+
+                                        <!--Website links to schools-->
+        <div class="row">
+            <div class="col-lg-4">
+                <img src="public/img/denhaag.jpg">
+                <i class="fas fa-users grey"></i>
+
+                <h2>Haagste</h2>
+                <p>
+                    Locatie Den Haag
+                </p>
+
+                <p><a class="btn" href="/overOns">Lees meer.</a></p>
+            </div>
+
+            <div class="col-lg-4">
+                <i class="fas fa-map grey"></i>
+
+                <h2>Locatie</h2>
+                <img src="public/img/zoetermeer.jpg">
+                <p>Locatie Zoetermeer</p>
+                <p><a class="btn" href="/contact">Lees meer.</a></p>
+            </div>
+            <div class="col-lg-4">
+                <img src="public/img/delft.jpg">
+                <i class="far fa-newspaper grey"></i>
+
+                <h2>Nieuws</h2>
+                <p>Locatie Delft</p>
+                <p><a class="btn" href="/nieuwsFeed">Lees meer.</a></p>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php
+include_once ('defaults/footer.php');
+?>
+</body>
+</html>
